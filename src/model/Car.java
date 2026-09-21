263f46d7662ae95cf392ea870f23ced9f79e9b10
Karuhito <179291384+Karuhito@users.noreply.github.com>
2026-09-21 17:19:04 +0900

    各modelクラスを一部修正

--- src/model/Car.java
+++ src/model/Car.java
@@ -2,87 +2,97 @@ package model;
 
 
 public class Car {
-	private int id;
-	private String vehicleModel;
-	private String color;
-	private CarMaker maker;
-	private CarStatus status;
-	private int cumulativeMileage;
-	private Integer currentUserId;
-	private CarTransmission transmission;
-	
-	
+  private int id;
+  private String vehicleModel;
+  private String color;
+  private CarMaker maker;
+  private CarStatus status;
+  private int cumulativeMileage;
+  private Integer currentUserId;
+  private CarTransmission transmission;
 
-	public Car(int id, String vehicleModel, String color, CarMaker maker, CarStatus status, int cumulativeMileage,
-			Integer currentUserId, CarTransmission transmission) {
-		this.id = id;
-		this.vehicleModel = vehicleModel;
-		this.color = color;
-		this.maker = maker;
-		this.status = status;
-		this.cumulativeMileage = cumulativeMileage;
-		this.currentUserId = currentUserId;
-		this.transmission = transmission;
-	}
+  /**
+   * コンストラクタ
+   * 
+   * @param id 車のID
+   * @param vehicleModel 車の車種名
+   * @param color 車の色
+   * @param maker 車のメーカー CarMaker
+   * @param status 車の状態。CarStatus
+   * @param cumulativeMileage 走行距離
+   * @param currentUserId 車を借りているユーザー 誰も借りていない場合はnull intではnullを表せないのでラッパークラスのIntegerで管理
+   * @param transmission 車のトランスミッションの判別 CarTransmission
+   */
+  public Car(int id, String vehicleModel, String color, CarMaker maker, CarStatus status,
+      int cumulativeMileage, Integer currentUserId, CarTransmission transmission) {
+    this.id = id;
+    this.vehicleModel = vehicleModel;
+    this.color = color;
+    this.maker = maker;
+    this.status = status;
+    this.cumulativeMileage = cumulativeMileage;
+    this.currentUserId = currentUserId;
+    this.transmission = transmission;
+  }
 
-	public String getVehicleModel() {
-		return vehicleModel;
-	}
+  public String getVehicleModel() {
+    return vehicleModel;
+  }
 
-	public void setVehicleModel(String vehicleModel) {
-		this.vehicleModel = vehicleModel;
-	}
+  public void setVehicleModel(String vehicleModel) {
+    this.vehicleModel = vehicleModel;
+  }
 
-	public String getColor() {
-		return color;
-	}
+  public String getColor() {
+    return color;
+  }
 
-	public void setColor(String color) {
-		this.color = color;
-	}
+  public void setColor(String color) {
+    this.color = color;
+  }
 
-	public CarMaker getMaker() {
-		return maker;
-	}
+  public CarMaker getMaker() {
+    return maker;
+  }
 
-	public void setMaker(CarMaker maker) {
-		this.maker = maker;
-	}
+  public void setMaker(CarMaker maker) {
+    this.maker = maker;
+  }
 
-	public CarStatus getStatus() {
-		return status;
-	}
+  public CarStatus getStatus() {
+    return status;
+  }
 
-	public void setStatus(CarStatus status) {
-		this.status = status;
-	}
+  public void setStatus(CarStatus status) {
+    this.status = status;
+  }
 
-	public int getCumulativeMileage() {
-		return cumulativeMileage;
-	}
+  public int getCumulativeMileage() {
+    return cumulativeMileage;
+  }
 
-	public void setCumulativeMileage(int cumulativeMileage) {
-		this.cumulativeMileage = cumulativeMileage;
-	}
+  public void setCumulativeMileage(int cumulativeMileage) {
+    this.cumulativeMileage = cumulativeMileage;
+  }
 
-	public CarTransmission getTransmission() {
-		return transmission;
-	}
+  public CarTransmission getTransmission() {
+    return transmission;
+  }
 
-	public void setTransmission(CarTransmission transmission) {
-		this.transmission = transmission;
-	}
+  public void setTransmission(CarTransmission transmission) {
+    this.transmission = transmission;
+  }
 
-	public int getId() {
-		return id;
-	}
+  public int getId() {
+    return id;
+  }
 
-	public void setCurrentUserId(Integer currentUserId) {
-		this.currentUserId = currentUserId;
-	}
+  public void setCurrentUserId(Integer currentUserId) {
+    this.currentUserId = currentUserId;
+  }
 
-	public Integer getCurrentUserId() {
-		return currentUserId;
-	}
-	
+  public Integer getCurrentUserId() {
+    return currentUserId;
+  }
+
 }
